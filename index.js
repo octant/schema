126d58829437ef0d47e91af6f169da1b3ce30d25
Michael Wood <mdwood@algomapublichealth.com>
2018-04-26 08:55:19 -0400

    chore: removed unnecessary file

--- index.js
+++
@@ -1,8 +0,0 @@
-import Schema from "./src/schema";
-import definition from "./schema-definition";
-
-const schema = new Schema(definition);
-
-const values = schema.defaultValues();
-console.log(values);
-console.log(schema.validate(values));
